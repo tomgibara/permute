ac0b130395f24347181d48deee92da15af5f0dc1
Tom Gibara <me@tomgibara.com>
2015-10-03 14:22:39 +0100

    Documents PermutationSequence.

--- src/main/java/com/tomgibara/permute/PermutationSequence.java
+++ src/main/java/com/tomgibara/permute/PermutationSequence.java
@@ -16,21 +16,67 @@
  */
 package com.tomgibara.permute;
 
+/**
+ * A sequence of permutations that can be iterated over forwards and backwards.
+ * Most methods on this interface return the sequence. This allows calls that
+ * modify the sequence position to be chained. All permutation sequences are
+ * associated with a permutation generator. The current permutation of the
+ * sequence can be obtained from this generator. Otherwise modifying the
+ * generator can also change the sequence position.
+ * 
+ * @author Tom Gibara
+ *
+ * @see Permutation.Generator
+ */
 public interface PermutationSequence {
 
+	/**
+	 * Whether there is a subsequent permutation in the sequence.
+	 * 
+	 * @return whether there is a next permutation
+	 */
 	boolean hasNext();
 
+	/**
+	 * Whether there is a prior permutation in the sequence.
+	 * 
+	 * @return whether there is a previous permutation
+	 */
 	boolean hasPrevious();
 
+	/**
+	 * Moves to the first in the sequence.
+	 * 
+	 * @return the sequence
+	 */
 	PermutationSequence first();
 
+	/**
+	 * Moves to the last in the sequence.
+	 * 
+	 * @return the sequence
+	 */
 	PermutationSequence last();
 
+	/**
+	 * Moves to the next in the sequence.
+	 * 
+	 * @return the sequence
+	 */
 	PermutationSequence next();
 
+	/**
+	 * Moves to the previous in the sequence.
+	 * 
+	 * @return the sequence
+	 */
 	PermutationSequence previous();
 
-	// the generator this sequence is manipulating
-
+	/**
+	 * The generator associated with this sequence. The current permutation of
+	 * the sequence can be obtained from this generator.
+	 * 
+	 * @return the sequence generator
+	 */
 	Permutation.Generator getGenerator();
 }
